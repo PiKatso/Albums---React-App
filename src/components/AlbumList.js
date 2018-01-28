dbfa28c7c0b3d3167dba2646ee95c51cd500a2a0
Kat Gifford <katgiff2007@gmail.com>
2018-01-28 11:28:14 -0800

    use lifecycle method 'componentWillMount' to make git request using the axios library

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,15 @@
 // Import the Libraries
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // Make the component
 class AlbumList extends Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
+  }
+
   render() {
     return (
       <View>
