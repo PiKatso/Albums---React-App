d675c606dd5af5b170466abdfebd9eddb38cb2d3
Kat Gifford <katgiff2007@gmail.com>
2018-01-27 16:02:03 -0800

    import header to index/root component

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import a library to creat a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header'
 
 // Create a component
 const App = () => {
