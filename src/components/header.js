82e4963caa3aff442d65589d23c8eeeb0f82aa1d
Kat Gifford <katgiff2007@gmail.com>
2018-01-27 16:35:54 -0800

    add additional header styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,34 @@
 // Import Libraries for making a Component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a Component
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    position: 'realative',
+    elevation: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60,
+    backgroundColor: '#fcfcfc',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.2,
+    paddingTop: 15
+  },
   textStyle: {
     fontSize: 20
   }
