58841f9901eb9fccd975e2c21d0c225c4d5b2f18
Kat Gifford <katgiff2007@gmail.com>
2018-01-28 12:25:41 -0800

    set up the child component AlbumDetail

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = () => {
+
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,9 @@
 // Import the Libraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail'
 
 // Make the component
 class AlbumList extends Component {
@@ -13,8 +15,9 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return
-    this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
