5f579fe3d77274d9bf1a12bc02be0e4aa404b8ee
Kat Gifford <katgiff2007@gmail.com>
2018-01-31 18:10:46 -0800

    add preliminary styles to the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,36 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle, buttonTextStyle } = styles;
+
   return (
-    <TouchableOpacity>
-      <Text>click me!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={buttonTextStyle}>
+        click me!
+      </Text>
     </TouchableOpacity>
   );
 };
 
 const styles = {
-
-}
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#a8d8da',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  buttonTextStyle: {
+    alignSelf: 'center',
+    color: '#a8d8da',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
 
 export default Button;
